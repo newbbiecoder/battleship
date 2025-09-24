04341088aa08a7f1d8f39f1696f53384ac6bb403
Rudraksh <rudrakshkumar2908@gmail.com>
2025-09-24 18:44:58 +0000

    Minor change to script.js

--- src/script.js
+++ src/script.js
@@ -42,9 +42,7 @@ class Gameboard {
         if(length === 1) {
             let [x,y] = coordinates;
 
-            if(this.board[x][y]) {
-                return "Already a ship present";
-            }
+            if(this.board[x][y]) return "Already a ship present";
             this.board[x][y] = ship;
         }
         else {
@@ -54,21 +52,17 @@ class Gameboard {
             if(x1 === x2) { // Vertical
                 if(y1 > 9 || y2 > 9) return "Invalid coord";
                 if(this.checkShipAlreadyPresent(y1,y2,x1) === false) return "Already a ship present";
-                for(;y1 <= y2; y1++) {
-                    this.board[x1][y1] = ship;
-                }
+                for(;y1 <= y2; y1++) this.board[x1][y1] = ship;
             }
             else if(y1 === y2){ // Horizontal
                 if(x1 > 9 || x2 > 9) return "Invalid coord";
                 if(this.checkShipAlreadyPresent(x1,x2,y1) === false) return "Already a ship present";
-                for(;x1 <= x2; x1++) {
-                    this.board[x1][y1] = ship;
-                }
+                for(;x1 <= x2; x1++) this.board[x1][y1] = ship;
             }
         }
     }
 
-    checkShipAlreadyPresent(a1,a2, b1, ship) {
+    checkShipAlreadyPresent(a1,a2, b1) {
         for(;a1 <= a2; a1++) {
             if(this.board[a1][b1]) return false;
         }
